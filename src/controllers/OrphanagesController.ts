0ffae35bfc5cce58e5de11232bce96d3c735634c
Felipe Rosa <flp.far@gmail.com>
2020-10-14 13:12:55 -0300

    Add orphanages index and show

---
+++ src/controllers/OrphanagesController.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from '../models/Orphanage';
+
+export default {
+  async index(request: Request, response: Response) {
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanages = await orphanagesRepository.find();
+
+    return response.status(200).json(orphanages);
+  },
+
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+    return response.status(200).json(orphanage);
+  },
+
+  async create(request: Request, response: Response) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      open_on_weekends,
+      opening_hours
+     } = request.body;
+  
+    const orphanagesRepository = getRepository(Orphanage);
+  
+    const orphanage = orphanagesRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      open_on_weekends,
+      opening_hours
+    });
+  
+    await orphanagesRepository.save(orphanage);
+  
+    return response.status(201).json(orphanage)
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,35 +1,11 @@
 import { Router } from 'express';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
+import OrphanagesController from './controllers/OrphanagesController';
+
 
 const routes = Router();
 
-routes.post('/orphanages', async (request, response) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    open_on_weekends,
-    opening_hours
-   } = request.body;
-
-  const orphanagesRepository = getRepository(Orphanage);
-
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    open_on_weekends,
-    opening_hours
-  });
-
-  await orphanagesRepository.save(orphanage);
-
-  return response.status(201).json(orphanage)
-});
+routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
+routes.post('/orphanages', OrphanagesController.create);
 
 export default routes;

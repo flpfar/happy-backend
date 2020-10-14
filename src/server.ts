aaf2a69a8b931acc252fc9b98b2a9b47016219af
Felipe Rosa <flp.far@gmail.com>
2020-10-14 18:20:55 -0300

    Add cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import path from 'path';
 import routes from './routes';
 import './database/connection';
 import errorHandler from './errors/handler';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));

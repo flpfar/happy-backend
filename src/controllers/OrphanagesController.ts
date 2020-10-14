d20374b5f1ca0349d9629c24fcd00ac06c108d29
Felipe Rosa <flp.far@gmail.com>
2020-10-14 18:19:11 -0300

    Add exception handler and validations

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import * as Yup from 'yup';
 import Orphanage from '../models/Orphanage';
 import orphanageView from '../views/orphanages_view';
 
@@ -43,8 +44,8 @@ export default {
      })
   
     const orphanagesRepository = getRepository(Orphanage);
-  
-    const orphanage = orphanagesRepository.create({
+
+    const data = {
       name,
       latitude,
       longitude,
@@ -53,7 +54,26 @@ export default {
       open_on_weekends,
       opening_hours,
       images
-    });
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(Yup.object().shape({
+        path: Yup.string().required()
+      }))
+    })
+
+    await schema.validate(data, {
+      abortEarly: false,
+    })
+  
+    const orphanage = orphanagesRepository.create(data);
   
     await orphanagesRepository.save(orphanage);
   
